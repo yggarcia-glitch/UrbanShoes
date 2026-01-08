2c24e0ac8dbaa7a2baaa83b19c8eec2c05369d90
yosef <yggarcia@sudamericano.edu.ec>
2026-01-08 18:51:40 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'UrbanShoes',
+  webDir: 'www'
+};
+
+export default config;
